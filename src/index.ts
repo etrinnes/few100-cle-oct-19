122077f2dad2693458ea17978b6dcf0f7c469169
etrinnes <etrinnes@users.noreply.github.com>
2019-10-15 15:52:11 -0400

    done with day 1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 
 import './styles.css';
+import { multiply } from './math';
 console.log('Ready to Party With Some TypeScript!');
 
+const number1El = document.getElementById('number1') as HTMLInputElement;
+const number2El = document.getElementById('number2') as HTMLInputElement;
+const multiplyButton = document.getElementById('multiply') as HTMLInputElement;
+const answerEl = document.getElementById('answer') as HTMLSpanElement;
+
+multiplyButton.addEventListener('click', function () {
+    const n1 = number1El.valueAsNumber;
+    const n2 = number2El.valueAsNumber;
+
+    const answer = multiply(n1, n2);
+
+    answerEl.innerHTML = answer.toString();
+});
